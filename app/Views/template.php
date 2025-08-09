d24f8c6a2ed3f8f915819da2c9ae319f53910437
jerica08 <jericamarquez88@gmail.com>
2025-08-09 21:27:11 +0800

    Navbar Update

--- app/Views/template.php
+++ app/Views/template.php
@@ -5,26 +5,22 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
     </head>
     <body>
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">WebSiteName</a>
+        <header>
+            <nav class="navbar" style="background-color:#1338BE;">
+                <div class="container-fluid d-flex justify-content-between align-items-center">
+                    <a class="navbar-brand text-white" href="#">Learning Management System</a>
+                    <ul class="nav d-flex align-items-center gap-3">
+                        <li class="nav-item"><a class="nav-link text-white" href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
                 </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">Page 1</a></li>
-                    <li><a href="#">Page 2</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                </ul>
-            </div>
-        </nav>
-    </body>
-    <section>
-        <div class="container">
-            <h1> Welcome to Learning Management System!</h1>
+</nav>
+        </header>
+         <section>
+        <div class="container" style="text-align:center; margin-top: 50px;">
+            <h1 >Welcome to the Learning Management System!</h1>
         </div>
     </section>
+    </body>
+   
 </html>
