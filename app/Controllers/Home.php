7e4b2ff6b5abfd26a37d8cb424f55a3e4682f499
jerica08 <jericamarquez88@gmail.com>
2025-08-09 20:00:08 +0800

    Home controller and view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,10 @@
 <?php
+define('BASEPATH') OR exit ('No direct script access allowed');
 
-namespace App\Controllers;
+class Home extends CI_Controller {
 
-class Home extends BaseController
-{
-    public function index(): string
-    {
-        return view('welcome_message');
+    public function index() {
+        $this->load->view('template');
     }
 }
+?>
