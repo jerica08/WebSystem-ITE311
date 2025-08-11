73879923b7a96c0af8efb9cee21008f04db82939
jerica08 <jericamarquez88@gmail.com>
2025-08-11 09:45:26 +0800

    navbar button short update

--- app/Views/template.php
+++ app/Views/template.php
@@ -4,14 +4,28 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
     </head>
+    <style>
+        .button{
+            background-color: #1338BE;
+            color : black;
+            border:none;
+            padding: 10px 20px;
+            cursor: pointer;
+            transition: background 0.3s;
+        }
+        .button:hover{
+            background-color: #0057b3a0;
+        }
+
+    </style>
     <body>
         <header>
             <nav class="navbar" style="background-color:#1338BE;">
                 <div class="container-fluid d-flex justify-content-between align-items-center">
-                    <a class="navbar-brand text-white" href="#">Learning Management System</a>
+                    <a class="navbar-brand text-white" href="#"><h2>Learning Management System</h2></a>
                     <ul class="nav d-flex align-items-center gap-3">
-                        <li class="nav-item"><a class="nav-link text-white" href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                        <li class="nav-item"><a class="nav-link text-white" href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#"><button class="button"> Sign Up</button></a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#"><button class="button"> Log-In</button></a></li>
                     </ul>
                 </div>
 </nav>
