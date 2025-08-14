98434b2a3f70f67aa14ef4dfa6d8e51b39ce4703
jerica08 <jericamarquez88@gmail.com>
2025-08-14 19:45:15 +0800

    courses schema

--- app/Database/Migrations/2025-08-14-113654_CreateCoursesTable.php
+++ app/Database/Migrations/2025-08-14-113654_CreateCoursesTable.php
@@ -8,11 +8,42 @@ class CreateCoursesTable extends Migration
 {
     public function up()
     {
-        //
+         $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'instructor_id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('instructor_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('courses');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('courses');
     }
 }
