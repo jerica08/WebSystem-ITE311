f0e995d4f36150bc10a22a1a0cfbd60d53531684
jerica08 <jericamarquez88@gmail.com>
2025-08-14 20:25:06 +0800

    seed sample data

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $userData = [[
+            'name' => 'jay',
+            'email' => 'jay@lms',
+            'pasSword' =>  password_hash('jay123', PASSWORD_DEFAULT),
+            'role' => 'admin',
+            'created_at' => date('Y-m-d H:i:s'),
+            'update_at' => date('Y-m-d H:i:s'),
+        ],
+        [
+            'name' => 'karl',
+            'email' => 'karl@lms',
+            'pasSword' =>  password_hash('karl123', PASSWORD_DEFAULT),
+            'role' => 'instructor',
+            'created_at' => date('Y-m-d H:i:s'),
+            'update_at' => date('Y-m-d H:i:s'),
+        ],
+        [
+            'name' => 'kendra',
+            'email' => 'kendra@lms',
+            'pasSword' =>  password_hash('kendra123', PASSWORD_DEFAULT),
+            'role' => 'student',
+            'created_at' => date('Y-m-d H:i:s'),
+            'update_at' => date('Y-m-d H:i:s'),
+        ]
+        ];
+    }
+}
