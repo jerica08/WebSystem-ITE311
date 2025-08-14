2cb1756e21ee9bda327932f2b5f93a8867bca8b7
jerica08 <jericamarquez88@gmail.com>
2025-08-14 20:05:12 +0800

    quizzes schema

--- app/Database/Migrations/2025-08-14-113753_CreateQuizzesTable.php
+++ app/Database/Migrations/2025-08-14-113753_CreateQuizzesTable.php
@@ -8,11 +8,38 @@ class CreateQuizzesTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'lesson_id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('lesson_id', 'lessons', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('quizzes');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('quizzes');
     }
 }
